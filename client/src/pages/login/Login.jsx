9c77bc0f7ac1f2bfaef9bb38a8ed08eb07c15da4
Izza303 <83118257+Izza303@users.noreply.github.com>
2022-12-03 14:32:13 +0500

    second commit

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,9 +1,26 @@
 import './login.scss'
-
 const Login = () => {
   return (
     <div className='login'>
-      login izza
+      <div className='card'>
+      <div className='left'>
+          <h1>
+           Grow Smart
+          </h1>
+          <p>Grow Smart is to help parents interact with other parents to discuss their child’s daily routine activities, find diet tips and cover all parent concerns on a single platform.  </p>
+          <span>Don't you have an account</span>
+          <button>Register</button>
+      </div>
+      <div className='right'>
+       <h1>Login</h1>
+      <form>
+        <input type="text" placeholder='Username'></input>
+        <input type="password" placeholder='Password'></input>
+        <button>Login</button>
+      </form>
+      </div>
+
+      </div>
     </div>
   )
 }
