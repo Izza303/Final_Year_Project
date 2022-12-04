f767b4ec1e8ed8fda1726eecfbb1132e9465fec8
faizaAman-2 <109220998+faizaAman-2@users.noreply.github.com>
2022-12-04 16:48:00 +0500

    navbar

--- client/src/components/leftBar/LeftBar.jsx
+++ client/src/components/leftBar/LeftBar.jsx
@@ -2,7 +2,21 @@ import "./leftBar.scss";
 function LeftBar() {
   return (
     <div className='leftBar'>
-        leftBar
+      <div className='container'>
+     <div className='menu'>
+    <div className='user'>
+    <img
+        src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <span>Faiza Aman</span>
+    </div>
+<div className='item'>
+  
+</div>
+     </div>
+      </div>
+      
       </div>
   )
 }
